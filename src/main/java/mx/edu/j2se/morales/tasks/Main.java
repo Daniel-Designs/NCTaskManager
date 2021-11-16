6eed8a4201e42e230697dbed25b46442c78e5da5
Daniel <tonystar.dm@gmail.com>
2021-11-16 13:06:28 -0600

    Commit Project Structure Whole Functionality without Java documentation.

--- src/main/java/mx/edu/j2se/morales/tasks/Main.java
+++ src/main/java/mx/edu/j2se/morales/tasks/Main.java
@@ -1,10 +1,22 @@
 package mx.edu.j2se.morales.tasks;
 
+import javax.sound.midi.Soundbank;
+import java.sql.SQLOutput;
+
 public class Main {
 	
 	public static void main(String[] args) {
 		System.out.println("Hola mundo");
-		System.out.println("Hola mundo 2");
+
+		Task Objeto1 = new Task("Prueba1",23);
+		Task Objeto3 = new Task("Prueba2",3,26,5);
+
+		Objeto1.setActive(true);
+		Objeto3.setActive(true);
+
+		System.out.println(Objeto1.nextTimeAfter(2));
+		System.out.println(Objeto3.nextTimeAfter(2));
+
 	}
 	
 }
---
+++ src/main/java/mx/edu/j2se/morales/tasks/Task.java
@@ -0,0 +1,140 @@
+package mx.edu.j2se.morales.tasks;
+
+import com.sun.javafx.binding.StringFormatter;
+
+public class Task {
+    //Atributos de la clase
+    private String title;
+    private int time;
+    private int start;
+    private int end;
+    private int interval;
+    private boolean active;
+    private boolean repeat;
+
+
+    //Constructor
+    public Task(String title, int time) {
+        this.title = title;
+        this.time = time;
+        this.repeat = false;
+        this.active = false;
+        this.start = 0;
+        this.end = 0;
+        this.interval = 0;
+    }
+
+    public Task(String title, int start, int end, int interval) {
+        this.title = title;
+        this.time = 0;
+        this.repeat = true;
+        this.active = false;
+        this.start = start;
+        this.end = end;
+        this.interval = interval;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getTime() {
+        return isRepeat() ? this.start : this.time ;
+    }
+
+    public void setTime(int time) {
+        if (isRepeat()) {
+            this.time = time;
+            this.repeat = true;
+        } else {
+            this.time = time;
+        }
+    }
+
+    public void setTime(int start, int end, int interval) {
+        if (isRepeat()) {
+            this.start = start;
+            this.end = end;
+            this.interval = interval;
+        } else {
+            this.start = start;
+            this.end = end;
+            this.interval = interval;
+            setRepeat(true);
+        }
+
+    }
+
+    public int getStart() {
+        return isRepeat() ? this.start : 0;
+    }
+
+    public void setStart(int start) {
+        this.start = start;
+    }
+
+    public int getEnd() {
+        return isRepeat() ? this.end : 0;
+    }
+
+    public void setEnd(int end) {
+        this.end = end;
+    }
+
+    public int getRepeatInterval() {
+        return isRepeat() ? this.interval : 0;
+    }
+
+    public void setInterval(int interval) {
+        this.interval = interval;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public boolean isRepeat() {
+        return repeat;
+    }
+
+    public void setRepeat(boolean repeat) {
+        this.repeat = repeat;
+    }
+
+    public int nextTimeAfter(int current) {
+        int nexTime = 0;
+
+        if (isActive()){
+
+            if(isRepeat()){
+
+                nexTime=getStart();
+
+                while (getStart()<current && current<getEnd()){
+
+                    nexTime += getRepeatInterval();
+
+                    if(current<nexTime && nexTime<getEnd()){
+                        return nexTime;
+                    }
+
+                }
+                return nexTime;
+
+            }else{
+                if(getTime()>current) {
+                    return getTime();
+                }
+            }
+    }
+        return -1;
+    }
+}
